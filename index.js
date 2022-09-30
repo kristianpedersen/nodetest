18c0b8f7f72b75832db309f50f02d4ab1b8705a5
Kristian Pedersen <furkrp@furst.no>
2022-09-30 13:29:23 +0200

    async-chunk-greier

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     res.writeHead(200, { "Content-Type": "text-plain" });
 
     console.clear();
-    console.log(req)
 
-    res.end("Det gikk bra")
+    const buffers = [];
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    const data = Buffer.concat(buffers).toString();
+
+    const { challenge } = JSON.parse(data);
+
+    console.log({ challenge })
+
+    res.end(challenge)
 });
 
 server.listen(process.env.PORT || 8443);
