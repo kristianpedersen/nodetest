945ce1c533786311330a30cc788ca3ff047da171
Kristian Pedersen <furkrp@furst.no>
2022-09-30 12:31:09 +0200

    hm

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     if (req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text-plain" });
-        res.end(greier)
+        res.end()
+        console.log("Mottok request")
     } else {
         res.writeHead(405, { "Content-Type": "text-plain" });
         res.end("Ikkje bra\n")
