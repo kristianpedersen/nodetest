f1b932af7eaf8031ff198482b7331a5642c250a1
Kristian Pedersen <furkrp@furst.no>
2022-09-30 11:48:15 +0200

    req-test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hei")
+const http = require("http");
+const server = http.createServer((req, res) => {
+    if (req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text-plain" });
+        res.end("Hei!\n")
+    } else {
+        res.writeHead(405, { "Content-Type": "text-plain" });
+        res.end("Ikkje bra\n")
+    }
+});
+
+server.listen(8443);
+console.log("Hei fra serveren")
