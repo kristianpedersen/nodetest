c69c63067643e8de5a5dd4f8878c1c4036895df5
Kristian Pedersen <furkrp@furst.no>
2022-09-30 12:34:53 +0200

    headers

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
     if (req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text-plain" });
         res.end()
-        console.log("Mottok request")
+        console.log(JSON.stringify(req.headers, null, 2))
     } else {
         res.writeHead(405, { "Content-Type": "text-plain" });
         res.end("Ikkje bra\n")
